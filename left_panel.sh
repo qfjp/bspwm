c829919c1599a158a1674821ff6b62bd373a6526
DJP <djpade@gmail.com>
2015-04-08 07:42:38 -0400

    Made panel settings monitor agnostic - added vars to panel shell scripts

--- left_panel.sh
+++ left_panel.sh
@@ -2,4 +2,4 @@
 cat $PANEL_FIFO \
     | $HOME/.config/bspwm/panel_dzen.sh \
     | dzen2 -p -ta l -bg '#303030' -fn 'Inconsolata for Powerline:Bold:size=10' \
-      -h $PANEL_HEIGHT -w 795 -sa c -x 5 -y 0 -e 'onstart=lower'
+      -h $PANEL_HEIGHT -w $PANEL_WIDTH -sa c -x 5 -y 0 -e 'onstart=lower'

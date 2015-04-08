09a9fcbc61e9759c8b46d716f3db7983b6711b5a
DJP <djpade@gmail.com>
2015-04-08 15:15:53 -0400

    Moved panel toggle state to separate directory

--- panel_settings.py
+++ panel_settings.py
@@ -2,6 +2,7 @@
 Constants related to bspwm panels
 """
 import utils
+import os
 
 HEIGHT = '18'
 FIFO = '/tmp/panel-fifo'
@@ -9,3 +10,4 @@ BG_COLOR = '#303030'
 FONT = 'Inconsolata for Powerline:Bold:size=10'
 WIDTH = '{}'.format(int(utils.get_x_resolution() / 2 - 5))
 OFFSET = '{}'.format(int(utils.get_x_resolution() / 2))
+TOG_FILE = '{}/.config/bspwm/state/panel_toggle'.format(os.environ['HOME'])
